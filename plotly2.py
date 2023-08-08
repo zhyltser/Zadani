a35e811748c923f01b8ccd8f917146b65b96bb34
zhyltser <141652599+zhyltser@users.noreply.github.com>
2023-08-08 16:42:10 +0200

    Update plotly2.py

--- plotly2.py
+++ plotly2.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
-import plotly
-# import plotly.express as px
 import plotly.express as px
+from PIL import Image
 
-N = 200
-M = 300
-numpy_image = np.random.randint(0, 256, size=(N, M, 3), dtype=np.uint8)
+image_path = '/Users/sergzhyltsov/Downloads/bunka.jpeg'
+image = cv2.imread(image_path)
+resolution = Image.open(image_path)
 
-fig = px.imshow(numpy_image)
-fig.update_layout(title='Barevný obrázek')
+np_image = np.array(image)
+N = resolution.width
+M = resolution.height
+
+np_image = cv2.resize(image, dsize=(N, M), interpolation=cv2.INTER_CUBIC)
+cv2.imwrite('new-image.jpeg', np_image)
+
+fig = px.imshow(np_image)
+fig.update_layout(title='Image')
 fig.show()
